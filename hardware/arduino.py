acfb8a4410221b3fe017c58e2ce43f1b90451e1b
Modi <narmagambetov05@gmail.com>
2026-04-16 23:18:39 +0800

    feat: add arduino control functions

--- hardware/arduino.py
+++ hardware/arduino.py
@@ -3,14 +3,18 @@ import dotenv
 
 arduino_serial = serial.Serial(dotenv.get_key(".env", "ARDUINO_PORT"), 115200)
 
-def set_arm_motor_position_up():
-    pass
+def ResetArmMotorPosition():
+    arduino_serial.write(bytes([0x10]))
 
-def reset_arm_motor_position():
-    pass
+def SetArmMotorPositionValue(pos):
 
-def reset_arm_motor_position():
-    pass
+    high = (pos>> 8) & 0xFF;
+    low = pos & 0xFF;
 
-def reset_arm_motor_position():
-    pass
+    arduino_serial.write(bytes([0x30, high, low]))
+    
+def OpenClaw():
+    arduino_serial.write(bytes([0x21]))
+
+def CloseClaw():
+    arduino_serial.write(bytes([0x22]))

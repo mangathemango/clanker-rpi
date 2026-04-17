c0e616e95a9545f80279ea4c5f63cfe989690f00
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-04-18 02:43:18 +0800

    chore: add new actions.py

--- actions.py
+++ actions.py
@@ -1,5 +1,8 @@
 import time
 from hardware import arduino, esp32
+from hardware.esp32 import esp32_serial
+# from cv.qr_read import qr_data
+import cv2
 
 def grabProp():
     # Placeholder for grab prop action - define as needed
@@ -72,6 +75,60 @@ def rotate_center(time_ds):
     move_motor(1, 100, time_ds)
     move_motor(2, -100, time_ds)
     move_motor(3, -100, time_ds)
+
+def move_right(time_ds):
+    move_motor(0, 100, time_ds)
+    move_motor(1, -100, time_ds)
+    move_motor(2, 100, time_ds)
+    move_motor(3, -100, time_ds)
     
+
+def put_material(index):
+    angle = 0
+    if index == 1:
+        angle = 93
+    arduino.OpenClaw()
+    esp32.set_angle(angle)
+    arduino.OpenClaw()
+    time.sleep(1)
+    arduino.SetArmMotorPositionValue(1800)
+    time.sleep(3)
+    arduino.CloseClaw()
+    time.sleep(1)
+    arduino.SetArmMotorPositionValue(2400)
+    time.sleep(3)
+    esp32.set_angle(40)
+    time.sleep(1)
+    arduino.SetArmMotorPositionValue(480)
+    time.sleep(6)
+    arduino.OpenClaw()
+    time.sleep(1)
+    arduino.SetArmMotorPositionValue(2400)
+    time.sleep(5)
+    arduino.CloseClaw()
+
+
+
+def main_path():
+    # move_diagonal(100, 15)
+    # move_forward(10)
+    # arduino.SetArmMotorPositionValue(1000)
+    # time.sleep(3)
+    # print(qr_data())
+    # move_forward(20)
+    # time.sleep(5)
+    arduino.CloseClaw()
+    arduino.SetArmMotorPositionValue(2000)
+    time.sleep(5)
+    arduino.OpenClaw()
+    time.sleep(1)
+    arduino.SetArmMotorPositionValue(2400)
+    # move_right(10)
+
+def read_qr_code():
+    esp32.set_angle(150)
+
 def main():
-    esp32.set_angle(0)
+    read_qr_code()
+
+    

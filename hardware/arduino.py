ca1e52ba107d562517cd22afa6a16bee029f8c9b
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-04-21 20:11:39 +0800

    fix: arduino and esp32.py not using global serials

--- hardware/arduino.py
+++ hardware/arduino.py
@@ -3,18 +3,22 @@ import dotenv
 
 arduino_serial = serial.Serial(dotenv.get_key(".env", "ARDUINO_PORT"), 115200)
 
+
 def ResetArmMotorPosition():
+    global arduino_serial
     arduino_serial.write(bytes([0x10]))
 
 def SetArmMotorPositionValue(pos):
-
+    global arduino_serial
     high = (pos>> 8) & 0xFF;
     low = pos & 0xFF;
 
     arduino_serial.write(bytes([0x30, high, low]))
     
 def OpenClaw():
+    global arduino_serial
     arduino_serial.write(bytes([0x21]))
 
 def CloseClaw():
+    global arduino_serial
     arduino_serial.write(bytes([0x22]))

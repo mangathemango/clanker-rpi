1c32c3e909ea3494d8b979109983edd26457b2af
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-04-25 20:20:52 +0800

    chore: very racist

--- cv/vision_ball.py
+++ cv/vision_ball.py
@@ -8,7 +8,7 @@ UPPER_RED1 = np.array([10, 255, 255])
 LOWER_RED2 = np.array([150, 120, 120])
 UPPER_RED2 = np.array([180, 255, 255])
 
-LOWER_GREEN = np.array([20, 40, 40])
+LOWER_GREEN = np.array([0, 0, 0])
 UPPER_GREEN = np.array([100, 255, 255])
 
 LOWER_BLUE = np.array([100, 100, 60])
